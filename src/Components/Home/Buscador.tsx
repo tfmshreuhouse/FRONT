cadb6ab29531c6ac483c1ff895f448f1c270e2d2
Katherine Vera <katherine.vera94@gmail.com>
2024-04-09 12:31:27 -0400

    vista home funcional

--- src/Components/Home/Buscador.tsx
+++ src/Components/Home/Buscador.tsx
@@ -3,12 +3,27 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { Dropdown } from 'primereact/dropdown';
 import axios from 'axios';
+import { InputNumber } from 'primereact/inputnumber';
 
-const DivHome = () => {
+interface Filtros {
+  costoMinimo: number | null;
+  costoMaximo: number | null;
+  tipoInmueble: number | undefined;
+  pais: string | undefined;
+  ciudad: string | undefined;
+  PAX: number | null;
+}
+interface ChildProps {
+  fetchPublicacionesFilter: (filtros:Filtros) => void;
+}
+
+const DivHome:React.FC<ChildProps> = ({fetchPublicacionesFilter}) => {
 
   const [tipoInmueble, setTipoInmueble] = useState(null);
   const [tipoInmuebleOptions, setTipoInmuebleOptions] = useState<{ value: number; label: string }[]>([]);
   const token: string | null = localStorage.getItem('jwt');
+  const [filtros, setFiltros] = useState<Filtros>({pais:undefined, ciudad:undefined, PAX: null, tipoInmueble: undefined,
+                                                    costoMaximo: null, costoMinimo: null});
 
   useEffect(() => { 
     cargarTipoInmueble();
@@ -18,7 +33,7 @@ const DivHome = () => {
       try {
 
           const response = await axios.get<{ success: boolean; data: { id: number; tipo: string }[] }>(
-              process.env.REACT_APP_API_URL + "rest/tipos-inmuebles"
+              process.env.REACT_APP_API_URL + "/rest/tipos-inmuebles"
           );
 
           const optionstipo = [];
@@ -42,74 +57,89 @@ const DivHome = () => {
         <h2>¡Encuentra tu próximo hogar con simples pasos!</h2>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">
+          <span className="p-field p-grid">
+          <label htmlFor="pais" className="p-col-fixed" style={{ width: '100%' }}>PAIS</label>   
             <InputText
               id="pais"
               name="pais"
+              value={filtros.pais}
+              onChange={(e) => {
+                setFiltros({...filtros, pais: e.target.value});
+                fetchPublicacionesFilter({...filtros, pais: e.target.value});
+              }}
             />
-            <label htmlFor="firstName">PAIS</label>
           </span>
-        </div>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">
+          <span className="p-field p-grid">
+          <label htmlFor="ciudad" className="p-col-fixed" style={{ width: '100%' }}>CIUDAD</label>   
             <InputText
               id="ciudad"
               name="ciudad"
+              value={filtros.ciudad}
+              onChange={(e) => {
+                setFiltros({...filtros, ciudad: e.target.value});
+                fetchPublicacionesFilter({...filtros, ciudad: e.target.value})
+              }}
             />
-            <label htmlFor="firstName">CIUDAD</label>
           </span>
-        </div>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">                                     
+          <span className="p-field p-grid">   
+            <label htmlFor="precio_minimo" className="p-col-fixed" style={{ width: '100%' }}>Tipo de Inmueble</label>                                  
               <Dropdown
-                  value={tipoInmueble}
+                  value={filtros.tipoInmueble}
                   options={tipoInmuebleOptions}
-                  onChange={(e) => setTipoInmueble(e.value)}                                        
+                  onChange={(e) => {setTipoInmueble(e.value); setFiltros({...filtros, tipoInmueble: e.target.value}); fetchPublicacionesFilter({...filtros, tipoInmueble: e.target.value})}}                                        
                   optionLabel="label"
                   placeholder="Seleccione"
                   className="TipoInmueble"
               />
-              <label>Tipo de Inmueble</label>
           </span>
-        </div>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">
-            <InputText
+          <span className="p-field p-grid">
+          <label htmlFor="precio_minimo" className="p-col-fixed" style={{ width: '100%' }}>PRECIO MINIMO</label>
+            <InputNumber
               id="precio_minimo"
               name="precio_minimo"
+              value={filtros.costoMinimo}
+              onChange={(e) => {
+                setFiltros({...filtros, costoMinimo: e.value});
+                fetchPublicacionesFilter({...filtros, costoMinimo: e.value})
+              }}
+              className="Precio" 
+              useGrouping={false} min={0} 
             />
-            <label htmlFor="firstName">PRECIO MINIMO</label>
           </span>
-        </div>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">
-            <InputText
-              id="precio_maximo"
-              name="precio_maximo"
-            />
-            <label htmlFor="firstName">PRECIO MAXIMO</label>
-          </span>
-        </div>
+      <span className="p-field p-grid">
+          <label htmlFor="precio_maximo" className="p-col-fixed" style={{ width: '100%' }}>PRECIO MAXIMO</label>
+          <InputNumber 
+                value={filtros.costoMaximo} 
+                onValueChange={(e: any) => {
+                  setFiltros({...filtros, costoMaximo: e.value});
+                  fetchPublicacionesFilter({...filtros, costoMaximo: e.value})
+                }} 
+                className="Precio" 
+                useGrouping={false} min={0} 
+          />
+      </span>
       </div>
       <div className="field col-12 lg:col-4">
-        <div className="form-button">
-          <span className="p-float-label">
-            <InputText
+          <span className="p-field p-grid">
+          <label htmlFor="precio_maximo" className="p-col-fixed" style={{ width: '100%' }}>CANTIDAD DE PERSONAS</label>
+            <InputNumber
               id="PAX"
               name="PAX"
+              value={filtros.PAX}
+              onChange={(e) => {
+                setFiltros({...filtros, PAX: e.value});
+                fetchPublicacionesFilter({...filtros, PAX: e.value})
+              }}
             />
-            <label htmlFor="firstName">CANTIDAD DE PERSONAS</label>
           </span>
-        </div>
       </div>
     </div>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,111 +1,150 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../index.css';
 import Card from '../Components/Home/Cards';
 import Buscadores from '../Components/Home/Buscador';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'primereact/button';
+import { Fieldset } from 'primereact/fieldset';
+import axios from 'axios';
+import ErrorAlert from '../Components/Shared/ErrorAlert ';
+
+interface ImagnenesInmuebles {
+  URL: string;
+  status: number;
+}
+
+interface Inmueble {
+  Nombre: string,
+  Pais: string;
+  Ciudad: string;
+  Direccion: string;
+  ImagnenesInmuebles: Array<ImagnenesInmuebles>;
+  User: User;
+}
+
+interface Publicaciones {
+  id: number,
+  indicaciones: string;
+  costo: number;
+  PAX: number;
+  descripcion: string;
+  image: string;
+  createdAt: string;
+  cantidadReservas: number;
+  InmuebleId: number;
+  Inmueble: Inmueble;
+}
+
+interface User {
+  id: number,
+  nombres: string;
+  apellidos: string;
+  correo: string;
+  perfil: string;
+  status: number;
+  telefono: string;
+}
+
+interface Filtros {
+  costoMinimo: number | null;
+  costoMaximo: number | null;
+  tipoInmueble: number | undefined;
+  pais: string | undefined;
+  ciudad: string | undefined;
+  PAX: number | null;
+}
 
 const Home = () => {
+
   const navigate = useNavigate();
-  const handleBoton1Click = (tipo: string, id: string) => {
+
+  const [filtros, setFiltros] = useState<Filtros>({pais:undefined, ciudad:undefined, PAX: null, tipoInmueble: undefined,
+                                                    costoMaximo: null, costoMinimo: null});
+  const [publicaciones, setPublicaciones] = useState<Publicaciones[]>([]);
+  const [failure, setFailure] = useState<boolean>(false);
+
+  const [first, setFirst] = useState(0); // Índice del primer elemento a mostrar
+  const [rows, setRows] = useState(5); // Número de filas por página
+  const [totalRecords, setTotalRecords] = useState(0); // Total de registros
+  const [currentPage, setCurrentPage] = useState(0); // Página actual
+
+  useEffect(() => { 
+    fetchPublicacionesFilter(filtros);
+  }, [first, rows]);
+
+  const fetchPublicacionesFilter = async (filtros:Filtros) => {
+    try {
+
+      const axiosTokenInfo = axios.create({
+        baseURL: process.env.REACT_APP_API_URL
+      });
+
+      const response = await axiosTokenInfo.post('/rest/publicacion/home/filter', filtros);
+      console.log(response.data.data);
+      setPublicaciones(response.data.data);
+
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      setFailure(true);
+    }
+  };
+
+  const handleErrorAlertClose = () => {
+    setFailure(false); 
+  };
+
+  const handleBoton1Click = (tipo: string, id: number) => {
     navigate(`/home/infoInmueble/${tipo}/${id}`);
   };
-  const handleBoton2Click = () => {
-    navigate(`/`);
+
+  const handleVolverClick = () => {
+    navigate('/');
   };
+
   return (
     <div className="container">
-      <Buscadores />
-      <div className="row">
-      <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://lajoya.ec/wp-content/uploads/2015/02/LJ-CONDOMINIO-2-SALA-COMEDOR-1108x960.png"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="1"
-          titulo="Apartamento maria paula"
-          precio="800.000 COP"
-          direccion="Cra 5 a 16-86"
-          pais="Colombia"
-          ciudad="Neiva"
+      <Fieldset>
+        <Buscadores fetchPublicacionesFilter={fetchPublicacionesFilter}/>
+        {publicaciones.length === 0 ? (
+          <div className="p-fluid grid">
+            <div className="field col-12 lg:col-4"></div>
+            <div className="field col-12 lg:col-4">
+              <h2>No hay publicaciones.</h2>
+            </div>
+            <div className="field col-12 lg:col-4"></div>
+          </div>
+          
+        ) : (
+          <>
+            <div className="row">
+              {publicaciones.map((publicacion, index) => (
+                <Card
+                  key={index}
+                  onBoton1Click={() => handleBoton1Click('1',publicacion.InmuebleId)}
+                  imageUrl={publicacion.Inmueble.ImagnenesInmuebles[0].URL} 
+                  buttonText="Ver detalles"
+                  className="col-md-4 col-sm-12"
+                  tipo="1"
+                  id={publicacion.InmuebleId + ""}
+                  titulo={publicacion.Inmueble.Nombre} 
+                  precio={publicacion.costo + " para " + publicacion.PAX + " personas"}
+                  direccion={publicacion.Inmueble.Direccion}
+                  pais={publicacion.Inmueble.Pais}
+                  ciudad={publicacion.Inmueble.Ciudad}
+                />
+              ))}
+            </div>
+          </>
+        )}
+      </Fieldset>
+      {failure && (
+        <ErrorAlert
+          header="Error"
+          text="No se pudo obtener las publicaciones, vuelva a intentarlo más tarde"
+          onClose={handleErrorAlertClose}
         />
-        <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://arquitectopablorestrepo.com/wp-content/uploads/2023/10/Casa-moderna-en-dos-pisos-construida-14-1.jpg"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="2"
-          titulo="Casa colinas del iga"
-          precio="1.500.000 COP"
-          direccion="Av. boyaca cll 55"
-          pais="Colombia"
-          ciudad="Bogota"
-        />
-        <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://img10.naventcdn.com/avisos/18/00/64/76/56/44/720x532/340390153.jpg?isFirstImage=true"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="3"
-          titulo="Habitacion"
-          precio="150.000 COP"
-          direccion="Cra 23 n 55"
-          pais="Paraguay"
-          ciudad="Asuncion"
-        />
-        <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://cf.bstatic.com/xdata/images/hotel/max1024x768/450159926.jpg?k=9b5bb60fad6df4fbbe81b9bd8ad0f49988e379fc1713442ce92a7c8507983d5c&o=&hp=1"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="4"
-          titulo="Aparta estudio"
-          precio="750.000 COP"
-          direccion="Cll 35 N 34-18"
-          pais="Mexico"
-          ciudad="Ciudad de Mexico"
-        />
-        <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/04/18/a2/0a/9-person-cabana-with.jpg?w=700&h=-1&s=1"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="5"
-          titulo="Cabaña frente al mar"
-          precio="2.550.000 COP"
-          direccion="playa del carmen"
-          pais="Mexico"
-          ciudad="Cancun"
-        />
-        <Card
-          onBoton1Click={handleBoton1Click}
-          imageUrl="https://i.pinimg.com/736x/21/b5/5a/21b55ac1a7d6a6b1cee1bb5db9cb8834.jpg"
-          buttonText="Ver detalles"
-          className="col-md-4 col-sm-12"
-          tipo="1"
-          id="6"
-          titulo="Piso en barcelona"
-          precio="4.000.000 COP"
-          direccion="cll 90 n 45-29"
-          pais="España"
-          ciudad="Barcelona"
-        />
-      </div>
-      <div className="col-md-12 mb-3 text-center">
-        <Button
-          type="button"
-          icon="pi pi-angle-left"
-          label="Volver"
-          className="button-red"
-          onClick={handleBoton2Click}
-        />
-      </div>
+      )}
     </div>
   );
 };
